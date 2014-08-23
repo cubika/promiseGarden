86130363d15f624e06ba5a85bf8c714c3174e80f
cubika <receiveresume@163.com>
2014-08-23 11:56:37 +0800

    structure

---
+++ README.md
@@ -0,0 +1,13 @@
+#Standard API
+## construcor
++ Promise(function resolver(resolve, reject){ ... })
+
+## instance method
++ then(onFulfilled, onRejected)
++ resolve(value)
++ reject(resason)
+
+## static method
++ Promise.resolve(value)
++ Promise.reject(reason)
++ Promise.all(array)
---
+++ shim.js
@@ -0,0 +1,29 @@
+var Resolver = function() {}
+
+function Promise(resolver) {
+	
+}
+
+Promise.prototype.then = function(onFulfilled, onRejected) {
+	
+}
+
+Promise.prototype.resolve = function(value) {
+	
+}
+
+Promise.prototype.reject = function(reason) {
+	
+}
+
+Promise.resolve = function(value) {
+	
+}
+
+Promise.reject = function(reason) {
+	
+}
+
+Promise.all = function(promises) {
+	
+}
